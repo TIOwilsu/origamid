a00f41178388d607b0a80237900f33f964313f22
tiowilsu <wilson.queiroz@outlook.com>
2021-06-01 23:20:42 -0300

    init react

---
+++ reactjs/webpack/webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
